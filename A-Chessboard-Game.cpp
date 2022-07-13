bc6d9e27a238c0a5612c8f13e82a1cc5051227b0
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-07-14 01:15:08 +0900

    Update A-Chessboard-Game.cpp

--- A-Chessboard-Game.cpp
+++ A-Chessboard-Game.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'chessboardGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x
+ *  2. INTEGER y
+ */
+
+string chessboardGame(int x, int y) {
+
+}
 
